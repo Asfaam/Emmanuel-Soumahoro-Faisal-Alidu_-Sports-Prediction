591d94f6a05dee43559bb08f4a883d44e1ba9254
“AFA <ashesifootball.contractarchive@gmail.com>
2023-10-22 21:25:00 +0000

    .

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.title("FIFA Player Rating Predictor")
 st.write("Enter player information to predict their overall rating.")
 
 # Create input fields for user data
-features = ['potential', 'age', 'value_eur', 'wage_eur', 'international_reputation', 'weak_foot', 'skill_moves', 'movement_acceleration', 'movement_sprint_speed', 'movement_agility', 'movement_reactions', 'movement_balance', 'power_shot_power', 'power_jumping', 'power_stamina', 'power_strength', 'power_long_shots', 'mentality_aggression', 'mentality_interceptions', 'mentality_positioning', 'mentality_vision']
+features = ['movement_reactions', 'mentality_composure', 'passing', 'potential', 'release_clause_eur', 'dribbling', 'wage_eur', 'power_shot_power', 'value_eur', 'mentality_vision', 'attacking_short_passing']
 input_data = {}
 for feature in features:
     input_data[feature] = st.number_input(f"Enter {feature}:", value=0)
