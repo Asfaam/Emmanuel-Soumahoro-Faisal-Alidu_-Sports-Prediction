6f1eb46b9f7585c7b31adb700eb3f3747df378df
“AFA <ashesifootball.contractarchive@gmail.com>
2023-10-22 20:58:47 +0000

    .

--- app.py
+++ app.py
@@ -3,14 +3,25 @@ import streamlit as st
 import pandas as pd
 
 # Load the trained model
-model_filename = 'player_rating_predictor.pkl'
-model = pickle.load(open(model_filename, 'rb'))
+# Define the model file path
+model_filename = 'C:/Users/user\OneDrive - Ashesi University/Desktop/ALL IN ONE/2ND YEAR, 2ND SEMESTER/Introduction to Artificial Intelligence/7. Week 9/AI_Final_MidSem_Project/player_rating_predictor.pkl'
+
+# Check if the model file exists
+try:
+    with open(model_filename, 'rb') as model_file:
+        model = pickle.load(model_file)
+except (FileNotFoundError, Exception) as e:
+    st.error(f"Error loading the model: {str(e)}")
 
 # Define a function for predicting player ratings
+# In the predict_player_rating function, ensure the model is a RandomForestRegressor
 def predict_player_rating(input_data):
-    input_data = input_data.values.reshape(1, -1)
-    prediction = model.predict(input_data)
-    return prediction[0]
+    if isinstance(model, RandomForestRegressor):
+        input_data = input_data.values.reshape(1, -1)
+        prediction = model.predict(input_data)
+        return prediction[0]
+    else:
+        st.error("Model is not a RandomForestRegressor")
 
 # Create a Streamlit web application
 st.title("FIFA Player Rating Predictor")
